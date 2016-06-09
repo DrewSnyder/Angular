b88a2a34419103e4884f7c3bb634d460fa8857fc
Drew M Snyder <Drew.Michael.Snyder@gmail.com>
2016-06-09 18:01:04 -0400

    Commit for Services

--- TourOfHeroes/app/app.component.ts
+++ TourOfHeroes/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import { OnInit } from '@angular/core';
+
 @Component({
     selector: 'my-app',
     template: 
@@ -62,16 +65,18 @@ styles:[`
     border-radius: 4px 0 0 4px;
   }
 `],
-directives: [HeroDetailComponent]
+//Directive tells Angular about the HeroDetailComponent
+directives: [HeroDetailComponent],
+providers: [HeroService]
 
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
     title = "Tour of Heroes";
 
     //Bring Access of HEROS variable to the AppComponent
-    public heroes = HEROES;
+    heroes : Hero[];
 
     //selected hero variable
     selectedHero: Hero;
@@ -80,19 +85,17 @@ export class AppComponent {
     onSelect(hero: Hero){
         this.selectedHero = hero;
     }
+
+    constructor(private heroService: HeroService){
+      
+    }
+
+    getHeros(){
+      this.heroService.getHeros().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(){
+      this.getHeros();
+    }
  }
 
-
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
